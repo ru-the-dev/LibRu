d6665e9c66bcc460644abdf32d3ae66b06a1494d
ru-the-dev <dev.ru.vero@gmail.com>
2026-01-25 10:18:21 +0100

    Refactor versioning logic in Core.lua to use static version numbers

--- Core.lua
+++ Core.lua
@@ -1,14 +1,9 @@
 local addon, ns = ...
 
--- create library
-local LibStub = _G.LibStub
-
--- Get the addon version (MAJOR, MINOR, PATCH)
-local version = C_AddOns.GetAddOnMetadata("LibRu", "Version")
-local parts = {strsplit(".", version)}
-local MAJOR = tonumber(parts[1]) or 2
-local MINOR = tonumber(parts[2]) or 0
-local PATCH = tonumber(parts[3]) or 0
+--- Define Library Version
+local MAJOR = 2
+local MINOR = 0
+local PATCH = 0
 
 -- Create a single version number for LibStub (MAJOR * 10000 + MINOR * 100 + PATCH)
 -- eg. for version 2.3.4, this will be 20000 + 300 + 4 = 20304
