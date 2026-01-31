42b25edbe6e19e64dcce4a613fe0676e2fbfe4c6
ru-the-dev <dev.ru.vero@gmail.com>
2026-01-31 22:24:56 +0100

    Reworked and improved Database system

---
+++ Database/API.lua
@@ -0,0 +1,103 @@
+local addon, ns = ...
+if ns.LibRu == nil then return end
+
+---@class LibRu
+local LibRu = ns.LibRu
+
+---@class LibRu.Database
+LibRu.Database = LibRu.Database or {}
+
+---@generic T
+---@class LibRu.Database.API
+---@field private _data T The live data table
+---@field private _defaults T
+---@field private _initialized boolean
+local DB_API = {}
+LibRu.Database.API = DB_API; 
+
+
+local function copyDefaults(dst, src)
+    for k, v in pairs(src) do
+        if type(v) == "table" then
+            dst[k] = dst[k] or {}
+            copyDefaults(dst[k], v)
+        elseif dst[k] == nil then
+            dst[k] = v
+        end
+    end
+end
+
+local function walkToParent(root, path)
+    local node = root
+    for i = 1, #path - 1 do
+        local k = path[i]
+        node[k] = node[k] or {}
+        node = node[k]
+    end
+    return node, path[#path]
+end
+
+---@generic T
+---@param self LibRu.Database.API|{_data: T}
+function DB_API:Init()
+    if self._initialized then return end
+    
+    copyDefaults(self._data, self._defaults)
+    self._initialized = true
+end
+
+---@generic T
+---@param self LibRu.Database.API|{_data: T}
+---@return T
+function DB_API:Get()
+    if self._initialized then return self._data end
+
+    self:Init();
+    return self._data
+end
+
+
+
+---@generic T
+---@param self LibRu.Database.API|{_data: T}
+function DB_API:ResetAll()
+    -- Clear the table keys without losing the reference
+    for k in pairs(self._data) do self._data[k] = nil end
+    copyDefaults(self._data, self._defaults)
+    return self._data
+end
+
+---@generic T
+---@param self LibRu.Database.API|{_data: T}
+function DB_API:ResetValue(path)
+    local db = self._data
+    local defs = self._defaults
+    for i = 1, #path - 1 do
+        defs = defs and defs[path[i]]
+    end
+    local parent, key = walkToParent(db, path)
+    local defVal = defs and defs[key]
+    if type(defVal) == "table" then
+        parent[key] = {}
+        copyDefaults(parent[key], defVal)
+    elseif defVal ~= nil then
+        parent[key] = defVal
+    else
+        parent[key] = nil
+    end
+    return parent[key]
+end
+
+---@generic T
+---@param self LibRu.Database.API|{_data: T}
+function DB_API:ResetSection(path)
+    local db = self._data
+    local defs = self._defaults
+    for i = 1, #path do
+        defs = defs and defs[path[i]]
+    end
+    local parent, key = walkToParent(db, path)
+    parent[key] = {}
+    if defs then copyDefaults(parent[key], defs) end
+    return parent[key]
+end
---
+++ Database/Database.lua
@@ -0,0 +1,28 @@
+local addon, ns = ...
+if ns.LibRu == nil then return end
+
+---@class LibRu
+local LibRu = ns.LibRu
+
+
+
+---@class LibRu.Database
+local Database = {};
+LibRu.Database = Database;
+
+---@generic T
+---@param dataTable table The live table (e.g. MySavedVar or {})
+---@param defaults T The template table
+---@return LibRu.Database.API|{data: T}
+function Database.Create(dataTable, defaults)
+    ---@type LibRu.Database.API
+    local instance = setmetatable({}, { __index = Database.API })
+    
+    -- Cast to any to bypass private field restrictions during setup
+    local internal = (instance --[[@as any]])
+    internal._data = dataTable
+    internal._defaults = defaults
+    internal._initialized = false
+    
+    return instance
+end
