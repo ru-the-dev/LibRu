f779482a9e2cbd2ae938e00a5561f8cf87255c10
ru-the-dev <dev.ru.vero@gmail.com>
2026-02-13 16:31:48 +0100

    feat: add LogContext and Logging modules for enhanced logging functionality

---
+++ Debug/LogContext.lua
@@ -0,0 +1,106 @@
+local addon, ns = ...
+if ns.LibRu == nil then return end
+
+---@class LibRu
+local LibRu = ns.LibRu
+
+if not LibRu.Colors then
+    error("LibRu.Colors is required for LibRu.Logging.LogContext")
+    return;
+end
+
+---@class LibRu.Logging
+LibRu.Logging = LibRu.Logging or {};
+
+---@alias LibRu.Logging.LogLevel "DISPLAY" | "INFO" | "WARNING" | "ERROR"
+LibRu.Logging.LOG_LEVELS = {
+    DISPLAY = {
+        Value = bit.lshift(1, 0),
+        Color = LibRu.Colors.All.White,
+    },
+    INFO = {
+        Value = bit.lshift(1, 1),
+        Color = LibRu.Colors.All.LightSkyBlue,
+    },
+    WARNING = {
+        Value = bit.lshift(1, 2),
+        Color = LibRu.Colors.All.Gold,
+    },
+    ERROR = {
+        Value = bit.lshift(1, 3),
+        Color = LibRu.Colors.All.Red,
+    }
+}
+
+--- ======================================================
+--- LibRu.LogContext
+--- ======================================================
+---@class LibRu.LogContext
+---@field Name string
+---@field LogLevelFlag number
+local LogContext = {}
+
+LibRu.Logging.LogContext = LogContext;
+LogContext.__index = LogContext;
+
+
+--- ======================================================
+--- Factory
+--- ======================================================
+
+---@param name string
+---@param ... LibRu.Logging.LogLevel Levels to enable for this context
+---@return LibRu.LogContext
+function LogContext.New(name, ...)
+    local instance = setmetatable({}, LogContext)
+
+    instance.Name = name or "Unnamed"
+    instance.LogLevelFlag = 0;
+
+    instance:EnableLevels(...)
+
+    return instance
+end
+
+--- ======================================================
+--- Configuration
+--- ======================================================
+
+---@param ... LibRu.Logging.LogLevel
+function LogContext:EnableLevels(...)
+    local levels = {...}
+    
+    for _, level in ipairs(levels) do
+        local levelDefinition = LibRu.Logging.LOG_LEVELS[level]
+        if levelDefinition then 
+            self.LogLevelFlag = bit.bor(self.LogLevelFlag, levelDefinition.Value)
+        end
+    end
+    
+end
+
+---@param ... LibRu.Logging.LogLevel
+function LogContext:DisableLevels(...)
+    local levels = {...}
+    
+    for _, level in ipairs(levels) do
+        local levelDefinition = LibRu.Logging.LOG_LEVELS[level]
+        if levelDefinition then 
+            self.LogLevelFlag = bit.band(self.LogLevelFlag, bit.bnot(levelDefinition.Value))
+        end
+    end
+end
+
+---@param ... LibRu.Logging.LogLevel
+---@return boolean
+function LogContext:AreLevelsEnabled(...)
+    local levels = {...}
+    for _, level in ipairs(levels) do
+        local levelDefinition = LibRu.Logging.LOG_LEVELS[level]
+        if not levelDefinition then return false end
+        if bit.band(self.LogLevelFlag, levelDefinition.Value) == 0 then
+            return false
+        end
+    end
+    return true
+end
---
+++ Debug/Logging.lua
@@ -0,0 +1,82 @@
+local addon, ns = ...
+if ns.LibRu == nil then return end
+
+---@class LibRu
+local LibRu = ns.LibRu
+
+if not LibRu.Logging.LogContext then
+    error("LibRu.Logging.LogContext is required for LibRu.Logging")
+    return;
+end
+
+--- ======================================================
+--- LibRu.Logging
+--- ======================================================
+
+--- @class LibRu.Logging
+local Logging = LibRu.Logging or {}
+LibRu.Logging = Logging;
+
+local globalContext = Logging.LogContext.New("LibRu",
+    "DISPLAY",
+    "INFO",
+    "WARNING",
+    "ERROR"
+);
+
+
+--- ======================================================
+--- Public API
+--- ======================================================
+
+
+function Logging.GetGlobalContext()
+    return globalContext
+end
+
+---@param level LibRu.Logging.LogLevel
+---@param message string
+---@param context? LibRu.LogContext
+function Logging.Log(level, message, context)
+    context = context or globalContext;
+
+    --- early out if the logging level is not enabled for this context.
+    if not context:AreLevelsEnabled(level) then return end
+
+    -- get the level definition (contains value and color)
+    local levelDefinition = Logging.LOG_LEVELS[level];
+    if not levelDefinition then 
+        error("Invalid log level: " .. level);   
+        return;
+    end;
+
+    -- format like so: [LEVEL] > [ContextName] : Message
+    local levelText = LibRu.Colors.ChatFormat(levelDefinition.Color, "[" .. level .. "]", false)
+    print(string.format("%s > [%s] : %s", levelText, context.Name, message))
+end
+
+---@param message string
+---@param context? LibRu.LogContext
+function Logging.LogDisplay(message, context)
+    Logging.Log("DISPLAY", message, context)
+end
+
+---@param message string
+---@param context? LibRu.LogContext
+function Logging.LogInfo(message, context)
+    Logging.Log("INFO", message, context)
+end
+
+---@param message string
+---@param context? LibRu.LogContext
+function Logging.LogWarning(message, context)
+    Logging.Log("WARNING", message, context)
+end
+
+---@param message string
+---@param context? LibRu.LogContext
+function Logging.LogError(message, context)
+    Logging.Log("ERROR", message, context)
+end
+
+
