2078c241f55fd1d48319d50c3bf73cab937b1889
ru-the-dev <dev.ru.vero@gmail.com>
2026-02-14 15:48:07 +0100

    feat: add Restrictions utility methods for managing add-on restrictions

--- Utils/Restrictions.lua
+++ Utils/Restrictions.lua
@@ -9,6 +9,7 @@ LibRu.Utils = LibRu.Utils or {};
 
 ---@class LibRu.Utils.Restrictions
 ---@field private _restrictions Enum.AddOnRestrictionType[]
+---@field private _restrictionSet table<Enum.AddOnRestrictionType, boolean>
 local Restrictions = {}
 LibRu.Utils.Restrictions = Restrictions
 
@@ -17,6 +18,13 @@ function Restrictions.New(...)
     local self = setmetatable({}, { __index = Restrictions })
     
     self._restrictions = { ... }
+
+    -- build a quick-lookup set to avoid O(n^2) contains checks
+    self._restrictionSet = {}
+    for _, v in ipairs(self._restrictions) do
+        self._restrictionSet[v] = true
+    end
+
     return self
 end
 
@@ -35,6 +43,20 @@ end
 function Restrictions:IsAnyActive()
     for _, restriction in ipairs(self._restrictions) do
         if C_RestrictedActions.IsAddOnRestrictionActive(restriction) then
+            return true
+        end
+    end
+
+    return false
+end
+
+---@param ... Enum.AddOnRestrictionType
+---@return boolean
+function Restrictions:Contains(...)
+    local restrictionsToCheck = { ... }
+    
+    for _, restriction in ipairs(restrictionsToCheck) do
+        if self._restrictionSet[restriction] then
             return true
         end
     end
